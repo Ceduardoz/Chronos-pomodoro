64aa4ee38a5352a26c6af2792318e22e546b97aa
Ceduardoz <ceduardoz957@gmail.com>
2025-09-11 14:17:51 -0300

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,20 @@
-import { BoltIcon, CirclePlay, CirclePlayIcon, HouseIcon, MoonIcon, PlayIcon, SunIcon, TableConfigIcon, TimerIcon, TimerResetIcon } from "lucide-react";
+import { Container } from "./components/Container/";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
 
 import "./styles/global.css";
 import "./styles/theme.css";
 
-export function App(){
+export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <header>
-            <button>
-              <TimerIcon/>
-              Chronos
-            </button>
-          </header>
-          <nav>
-              <button>
-                <HouseIcon/>
-              </button>
-              <button>
-                <TimerResetIcon/>
-              </button>
-              <button>
-                <BoltIcon />
-              </button>
-              <button>
-                <SunIcon/>
-              </button>
-          </nav>
-          <section className="timer"></section>
-          <div className="task">
-            <p>task</p>
-            <input type="text" placeholder="Ex: estudar para prova"/>
-            <p>Nesse inicio <strong>foque</strong> por <strong>25 min.</strong></p>
-            <button>
-              <CirclePlayIcon/>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
