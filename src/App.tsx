9db71983ac7f17d32ad493bd2586935e67ac0473
ceduardoz <ceduardoz957@gmail.com>
2026-03-06 22:47:07 -0300

    refactor: reorganize context structure by destructuring index and separating modules into folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from "react";
-
 import Home from "./pages/Home";
-
-import type { TaskStateModel } from "./models/TaskStateModel";
-import { TaskContext, TaskContextProvider } from "./contexts/TaskContext";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 import "./styles/global.css";
 import "./styles/theme.css";
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTasks: null,
-  currentCycle: 0,
-  config: {
-    worktime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 export function App() {
-  const [state, setState] = useState(initialState);
   return (
-    <>
-      <TaskContextProvider value={{ outracoisa: 321 }}>
-        <Home />
-      </TaskContextProvider>
-    </>
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
   );
 }
